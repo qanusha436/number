9fda8bffb27ea408777e92d4f43eaec610f95041
qanusha436 <qanusha436@gmail.com>
2025-06-11 12:53:27 +0530

    saved

--- positive.java
+++ positive.java
@@ -1,12 +1,5 @@
 package ESA;
 
 public class positive {
-    public static void main(String[] args) {
-        int num=5;
-        
-        if (num > 0) {
-            System.out.println(num + " is positive.");
-        } 
-    }
+
 }
- 
