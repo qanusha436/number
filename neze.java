c0e284dd82af83cfda0e551f6f7ae8b1810c688b
qanusha436 <qanusha436@gmail.com>
2025-06-11 12:58:39 +0530

    saved

---
+++ neze.java
@@ -0,0 +1,14 @@
+package ESA;
+
+public class neze {
+    public static void main(String[] args) {
+        int num = -3;
+
+        if (num <=0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
+    }
+
+}
